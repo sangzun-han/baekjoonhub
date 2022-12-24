17564c25c2f347ff6e8a659862066a33132071e5
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2022-12-24 20:12:53 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2288 ms, Memory: 94380 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static boolean[] visited;
+    static int ans[];
+    static ArrayList<Integer>[] tree;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N+1];
+        ans = new int[N+1];
+        tree = new ArrayList[N+1];
+
+        for(int i=0; i<=N; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<N-1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            tree[x].add(y);
+            tree[y].add(x);
+        }
+        dfs(1);
+        for(int i=2; i<=N; i++) {
+            System.out.println(ans[i]);
+        }
+    }
+
+    private static void dfs(int num) {
+        visited[num] = true;
+        for(int n: tree[num]) {
+            if(!visited[n]) {
+                ans[n] = num;
+                dfs(n);
+            }
+        }
+    }
+}
