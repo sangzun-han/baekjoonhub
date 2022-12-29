af04bb1de37ef8c4f15e576ea14c2c869892d1e7
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2022-12-29 17:15:54 +0900

    [Silver II] Title: 섬의 개수, Time: 132 ms, Memory: 13468 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int w,h;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1,-1,0,1,1,1,0,-1};
+    static int[] dy = {0,1,1,1,0,-1,-1,-1};
+    static class Point {
+        int x,y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            if(w==0 && h==0) break;
+            map = new int[h][w];
+            visited = new boolean[h][w];
+            for(int i=0; i<h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            int ans = 0;
+            for(int i=0; i<h; i++) {
+                for(int j=0; j<w; j++) {
+                    if(map[i][j]==1 && !visited[i][j]) {
+                        bfs(i,j);
+                        ans++;
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.offer(new Point(x,y));
+        visited[x][y] = true;
+
+        while(!queue.isEmpty()) {
+            Point p = queue.poll();
+
+            for(int d=0; d<8; d++) {
+                int nx = p.x + dx[d];
+                int ny = p.y + dy[d];
+
+                if(nx>=0 & nx<h && ny>=0 && ny<w && !visited[nx][ny] && map[nx][ny]==1) {
+                    queue.offer(new Point(nx,ny));
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+
+    }
+}
