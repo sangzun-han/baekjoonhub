2ed3ceed02dbc9644ca49c88d8ded0d8331a169f
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-02-20 00:58:34 +0900

    [unrated] Title: 카드 뭉치, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+        int j = 0;
+        int k = 0;
+        for(int i=0; i<goal.length; i++) {
+            if(j < cards1.length && goal[i].equals(cards1[j])){
+                j++;
+            } else if (k < cards2.length && goal[i].equals(cards2[k])) {
+                k++;
+            } else {
+                answer = "No";
+            }
+        }
+        return answer;
+    }
+}
