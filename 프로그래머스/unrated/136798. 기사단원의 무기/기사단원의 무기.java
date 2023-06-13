ccf6039ea187f5de15b637595354f2ff13288044
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-13 20:48:02 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.69 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -3,12 +3,12 @@ class Solution {
         int answer = 0;
         
         for(int i=1; i<=number; i++) {
-            int cnt = 0;
+            int count = 0;
             for(int j=1; j*j<=i; j++) {
-                if(j*j==i) cnt++;
-                else if(i%j==0) cnt+=2;
+                if(j*j==i) count++;
+                else if(i%j==0) count+=2;
             }
-            answer += cnt > limit ? power: cnt;
+            answer += count > limit ? power : count;
         }
         return answer;
     }
