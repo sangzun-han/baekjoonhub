48a58170cc5150ec7da1e51fa07a8033579f1a84
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-13 20:39:20 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.17 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,37 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        StringBuilder sb = new StringBuilder();
-        Queue<Integer> queue = new ArrayDeque<>();
         Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         
         for(int i=1; i<food.length; i++) {
-            if(food[i]/2==0) continue;
-            if(food[i]/2==0) {
-                for(int j=0; j<=food[i]/2; j++) {
+            for(int j=0; j<food[i]/2; j++) {
                     stack.push(i);
-                    queue.offer(i);
-                }
-            } else {
-
-                for(int j=0; j<=food[i]/2-1; j++) {
-                    stack.push(i);
-                    queue.offer(i);
-                }
+                    queue.offer(i);        
             }
+
         }
         
         while(!queue.isEmpty()) {
             sb.append(queue.poll());
         }
+        
         sb.append(0);
         
         while(!stack.isEmpty()) {
             sb.append(stack.pop());
         }
         
-        answer = sb.toString();
+        String answer = sb.toString();
         return answer;
     }
 }
