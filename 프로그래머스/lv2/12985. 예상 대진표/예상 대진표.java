11803d695aa5dd3dd8faec33f58545288af75ea1
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-19 21:44:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -1,20 +1,20 @@
 class Solution {
-    static int N = 0;
-    static int A = 0;
-    static int B = 0;
+    static int A=0;
+    static int B=0;
+    
     public int solution(int n, int a, int b) {
-        int answer = 0;
-        int N = n;
-        int A = a;
-        int B = b;
-        while(Math.abs(A-B)!=1 || Math.max(A,B)%2==1) {
-            answer++;
-            if(A%2==0)  A = A/2;
-            else if(A%2==1) A = A/2+1;
-            if(B%2==0) B = B/2;
-            else if(B%2==1) B = B/2+1;
-        }
+        A = a;
+        B = b;
         
-        return answer+1;
+        int answer = 1;
+        
+        while(Math.abs(B-A)!=1 || Math.max(A,B)%2==1) {
+           
+            A = A % 2 == 0 ? A/2 : A/2+1;
+            B = B % 2 == 0 ? B/2 : B/2+1;
+            answer++;
+        }
+
+        return answer;
     }
 }
