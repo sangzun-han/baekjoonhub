9f0824f56f37e78457a5a0293c190fc3c39b094d
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-23 23:07:39 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.24 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -4,27 +4,26 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         Stack<Character> stack = new Stack<>();
-        
-        for(int i=0; i<s.length()-1; i++) {
-            stack.clear();
+        for(int d=0; d<s.length(); d++) {
             boolean flag = false;
-            
-            for(int j=0; j<s.length(); j++) {
-               if(s.charAt(j)=='[' || s.charAt(j)=='(' || s.charAt(j)=='{') {
-                    stack.push(s.charAt(j));
+            for(int i=0; i<s.length(); i++) {
+                if(s.charAt(i)=='[' || s.charAt(i)=='{' || s.charAt(i)=='(') {
+                    stack.push(s.charAt(i));
                     flag = true;
-                } else if(!stack.isEmpty() &&s.charAt(j)==']' && stack.peek()=='[') {
+                } else if(!stack.isEmpty() && s.charAt(i)==']' && stack.peek()=='[') {
                     stack.pop();
-                } else if(!stack.isEmpty() &&s.charAt(j)=='}' && stack.peek()=='{') {
+                } else if(!stack.isEmpty() && s.charAt(i)=='}' && stack.peek()=='{') {
                     stack.pop();
-                } else if(!stack.isEmpty() &&s.charAt(j)==')' && stack.peek()=='(') {
+                } else if(!stack.isEmpty() && s.charAt(i)==')' && stack.peek()=='(') {
                     stack.pop();
-                }
+                }   
             }
-            
             if(flag && stack.isEmpty()) answer++;
             s = s.substring(1,s.length()) + s.substring(0,1);
+            stack.clear();
         }
+            
+    
         return answer;
     }
 }
