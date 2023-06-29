d7f16db2534ca0c8b85b270e8f58c3dac2c4eed6
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-29 16:40:40 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -2,33 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
+
         int answer = 0;
-        // 사야될 물건과 개수
-        HashMap<String,Integer> wantList = new HashMap<>();
-        // x ~ x+10일간 사게될 물건과 개수
-        HashMap<String,Integer> buyList = new HashMap<>();
+        HashMap<String, Integer> wantList = new HashMap<>();
+        HashMap<String, Integer> buyList = new HashMap<>();
         
         for(int i=0; i<want.length; i++) {
             wantList.put(want[i], number[i]);
         }
         
-        
-        for(int i=0; i<discount.length-9; i++) {
-            buyList.clear();
+        for(int i=0; i<discount.length - 9; i++) {
             for(int j=0; j<10; j++) {
-                buyList.put(discount[i+j], buyList.getOrDefault(discount[i+j],0)+1);
+                buyList.put(discount[i+j], buyList.getOrDefault(discount[i+j],0) + 1);   
             }
             
             boolean flag = true;
-            
-            for(String buyItem: buyList.keySet()) {
-                if(wantList.get(buyItem) != buyList.get(buyItem)) {
+            for(String product: wantList.keySet()) {    
+                if(wantList.get(product) != buyList.get(product)) {
                     flag = false;
                     break;
                 }
             }
             
             if(flag) answer++;
+            buyList.clear();
         }
         
         return answer;
