361acc74deafc2923b3e31283fc637714ce70f55
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-29 12:01:23 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -6,18 +6,13 @@ class Solution {
         HashMap<String, Integer> hashMap = new HashMap<>();
         
         for(int i=0; i<clothes.length; i++) {
-            if(hashMap.containsKey(clothes[i][1])) {
-                int count = hashMap.get(clothes[i][1]);
-                hashMap.put(clothes[i][1],count+1);
-            }  else {
-                hashMap.put(clothes[i][1], 1);
-            }
+            hashMap.put(clothes[i][1], hashMap.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        for(String value: hashMap.keySet()) {
-            answer = answer * (hashMap.get(value)+1);
+        for(String str: hashMap.keySet()) {
+           answer = answer * (hashMap.get(str)+1);
         }
+        
         return answer-1;
     }
 }
-
