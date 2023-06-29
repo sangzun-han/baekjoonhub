f3231f0b61162eb149a39a6cd486de267ee6a6d2
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2023-06-29 15:54:23 +0900

    [level 2] Title: 튜플, Time: 0.90 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        String [] strs = s.replaceAll("[{}]", " ").trim().split(" ,");
+       
+        String[] strs = s.replaceAll("[{}]", " ").trim().split(" ,");
         int[] answer = new int[strs.length];
         HashSet<Integer> set = new HashSet<>();
-        Arrays.sort(strs, (a,b)->(a.length()-b.length()));
+        Arrays.sort(strs, (a,b) -> (a.length() - b.length()));
         int index = 0;
         for(String str: strs) {
             for(String x: str.split(",")) {
@@ -13,10 +14,9 @@ class Solution {
                 if(set.contains(num)) continue;
                 set.add(num);
                 answer[index++] = num;
-
             }
         }
-     
+
         return answer;
     }
 }
