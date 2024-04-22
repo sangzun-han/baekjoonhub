6ad5e0a3757a307fc1e840ed7cb1d5621be2fa64
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2024-04-22 20:29:15 +0900

    [Silver IV] Title: ATM, Time: 88 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,27 +1,42 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] p = new int[n];
-		int[] ans = new int[n];
-		int answer = 0;
-		int sum = 0;
-		for(int i=0; i<n; i++) {
-			p[i] = sc.nextInt();
-		}
-		Arrays.sort(p);
-	
-		for(int i=0; i<n; i++) {
-			sum+=p[i];
-			ans[i] = sum;
-		}
-		
-		for(int i=0; i<n; i++) {
-			answer += ans[i];
-		}
-		
-		System.out.print(answer);
-	}
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st  = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] times = new int[N];
+        int[] ans = new int[N];
+        int sum = 0;
+        int answer = 0;
+        for(int i=0; i<N; i++) {
+            int P = Integer.parseInt(st.nextToken());
+            times[i] = P;
+
+        }
+
+        Arrays.sort(times);
+
+
+        for(int i=0; i<N;i ++) {
+            sum += times[i];
+            ans[i] = sum;
+        }
+
+        for(int i=0; i<N; i++) {
+            answer += ans[i];
+        }
+
+        System.out.println(answer);
+
+
+
+
+
+    }
 }
+
