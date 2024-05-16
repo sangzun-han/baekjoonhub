4d80b3f58d3f5a43689e1d074f792fa4ecf4bf50
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2024-05-17 02:00:09 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1408 ms, Memory: 78204 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,39 +3,42 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static boolean[] visited;
-    static int ans[];
     static ArrayList<Integer>[] tree;
+    static boolean[] visited;
+    static int[] ans;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
+
+        tree = new ArrayList[N+1];
         visited = new boolean[N+1];
         ans = new int[N+1];
-        tree = new ArrayList[N+1];
+
+
 
         for(int i=0; i<=N; i++) {
             tree[i] = new ArrayList<>();
         }
 
+
         for(int i=0; i<N-1; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            tree[x].add(y);
-            tree[y].add(x);
-        }
-        dfs(1);
-        for(int i=2; i<=N; i++) {
-            sb.append(ans[i]).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            tree[a].add(b);
+            tree[b].add(a);
         }
 
-        System.out.println(sb);
+        dfs(1);
+        for(int i=2; i<=N; i++) {
+            System.out.println(ans[i]);
+        }
     }
 
     private static void dfs(int num) {
         visited[num] = true;
+
         for(int n: tree[num]) {
             if(!visited[n]) {
                 ans[n] = num;
