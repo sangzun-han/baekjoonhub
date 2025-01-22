a4df6d944e660033baa013f8eea269021b88cb77
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-01-22 15:26:19 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 29.45 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,40 +1,36 @@
 class Solution {
     public int solution(int[] mats, String[][] park) {
         int answer = -1;
-        int x = park.length;
-        int y = park[0].length;
+        int n = park.length;
+        int m = park[0].length;
         
-        for(int i=0; i<mats.length; i++) {
-            int mat = mats[i];
+        for(int mat: mats) {
             
-            for(int j=0; j<x; j++) {
-                for(int k=0; k<y; k++) {
-                    if(park[j][k].equals("-1")) {
-                        answer = Math.max(answer, check(park, j,k, mat));
-                    }
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<m; j++) {
+                   if(park[i][j].equals("-1")) {
+                       int count = calc(i,j,park,mat);
+                       if(count == mat*mat) {
+                           answer = Math.max(answer, mat);
+                       }
+                   }
                 }
             }
         }
         return answer;
     }
     
-    private int check(String[][] park, int x, int y, int mat) {
+    private int calc(int x, int y, String[][] park, int mat) {
+        int count = 0;
         int n = park.length;
         int m = park[0].length;
-        // x,y 시작점
-        int count = 0;
+        
         for(int i=x; i<x+mat; i++) {
             for(int j=y; j<y+mat; j++) {
-                if(i<n && j<m) {
-                    if(!park[i][j].equals("-1")) {
-                        return -1;
-                    }
-                    count++;
-
-                } 
+                if(i>=n || j>=m) return -1;
+                else if(park[i][j].equals("-1")) count++;
             }
         }
-        return (count == mat * mat) ? mat : -1;
- 
+        return count;
     }
 }
