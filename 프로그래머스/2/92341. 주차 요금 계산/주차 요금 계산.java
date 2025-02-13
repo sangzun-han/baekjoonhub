17c4e97143d2cd84c68a441926ce60e2e4b3f889
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-02-13 18:28:44 +0900

    [level 2] Title: 주차 요금 계산, Time: 8.64 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,55 +2,65 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
+        TreeMap<Integer, List<Integer>> carRecord = new TreeMap<>();
         List<Integer> list = new ArrayList<>();
-        Map<Integer, List<Integer>> recordMaps = new TreeMap<>();
         
         int primaryTime = fees[0];
         int primaryPrice = fees[1];
-        int timePer = fees[2];
-        int pricePer = fees[3];
+        int perTime = fees[2];
+        int perPrice = fees[3];
         
         for(int i=0; i<records.length; i++) {
-            String[] recordSplit = records[i].split(" ");
-            int time = minuteFromTime(recordSplit[0]);
-            int carNumber = Integer.parseInt(recordSplit[1]);
-            List<Integer> recordMapsList = recordMaps.getOrDefault(carNumber, new ArrayList<>());
-            recordMapsList.add(time);
-            recordMaps.put(carNumber, recordMapsList);
+            String[] record = records[i].split(" ");
+            int carNumber = Integer.parseInt(record[1]);
+            int time = convertTime(record[0]);
+            
+            List<Integer> timeRecord = carRecord.getOrDefault(carNumber, new ArrayList<>());
+            timeRecord.add(time);
+            carRecord.put(carNumber, timeRecord);
         }
         
-        for(int carNumber : recordMaps.keySet()) {
-            
+        for(int carNumber: carRecord.keySet()) {
+            int totalPrice = 0;
             int totalTime = 0;
-            
-            List<Integer> parkTime = recordMaps.get(carNumber);
-            if(parkTime.size()%2 == 1) {
-                parkTime.add(minuteFromTime("23:59"));
+            List<Integer> times = carRecord.get(carNumber);
+            // 출차가 없다.
+            if(times.size()%2==1) {
+                times.add(convertTime("23:59"));
             }
             
-            for(int i=0; i<parkTime.size()-1; i+=2) {
-                totalTime += parkTime.get(i+1) - parkTime.get(i);
+            for(int i=0; i<times.size(); i+=2) {
+                int inTime = times.get(i);
+                int outTime = times.get(i+1);
+                totalTime += outTime - inTime;   
             }
             
-            int price = primaryPrice;
+            totalPrice = primaryPrice;
+            totalTime -= primaryTime;
             
-            if(totalTime > primaryTime) {
-                int remainTime = totalTime - primaryTime;
-                price += remainTime / timePer * pricePer;
-                if(remainTime % timePer > 0) price += pricePer;
+            if(totalTime > 0) {
+                if(totalTime % perTime != 0) {
+                    totalPrice += (totalTime / perTime) * perPrice + perPrice;
+                } else {
+                    totalPrice += (totalTime / perTime) * perPrice;
+                }  
             }
-            list.add(price);
+            list.add(totalPrice);
         }
         
         int[] answer = new int[list.size()];
         for(int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
+        
         return answer;
     }
     
-    public int minuteFromTime(String time) {
+    public int convertTime(String time) {
         String[] timeSplit = time.split(":");
-        return (Integer.parseInt(timeSplit[0])*60) + Integer.parseInt(timeSplit[1]);
+        int hour = Integer.parseInt(timeSplit[0]);
+        int minute = Integer.parseInt(timeSplit[1]);
+        
+        return hour * 60 + minute;
     }
 }
