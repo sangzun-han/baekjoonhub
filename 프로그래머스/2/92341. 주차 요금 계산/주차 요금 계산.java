e2e4b7b42dade0c5a4c7fae053eff15dfb920f3c
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-02-13 18:39:23 +0900

    [level 2] Title: 주차 요금 계산, Time: 8.53 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -51,7 +51,7 @@ class Solution {
         int[] answer = new int[list.size()];
         for(int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
-        }
+        }   
         
         return answer;
     }
