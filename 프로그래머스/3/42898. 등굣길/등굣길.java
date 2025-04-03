8fa7b1a512dd26db9cd2ebc83a60bf39e7d50ee2
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-03 20:46:17 +0900

    [level 3] Title: 등굣길, Time: 0.93 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,31 +1,32 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
+        int[][] dp = new int[n][m];
         boolean[][] isPuddle = new boolean[n][m];
-        int[][] d = new int[n][m];
-        for(int[] puddle: puddles) {
-            int r = puddle[0]-1;
-            int c = puddle[1]-1;
-            isPuddle[c][r] = true;
-        }
         
-        for(int i=0; i<n; i++) {
-            if(isPuddle[i][0]) break;
-            d[i][0] = 1;
+        for(int[] puddle: puddles) {
+            int c = puddle[0]-1;
+            int r = puddle[1]-1;
+            isPuddle[r][c] = true;    
         }
         
         for(int i=0; i<m; i++) {
             if(isPuddle[0][i]) break;
-            d[0][i] = 1;
+            else dp[0][i] = 1;
+        }
+        
+        for(int i=0; i<n; i++) {
+            if(isPuddle[i][0]) break;
+            else dp[i][0] = 1;
         }
         
         for(int i=1; i<n; i++) {
             for(int j=1; j<m; j++) {
-                if(isPuddle[i][j]) d[i][j] = 0;
-                else d[i][j] = (d[i-1][j]+d[i][j-1])%1000000007;
+                if(isPuddle[i][j]) dp[i][j] = 0;
+                else dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1_000_000_007;
             }
         }
         
-        return d[n-1][m-1];
+        
+        return dp[n-1][m-1];
     }
 }
