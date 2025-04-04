338ba7c448b0ac89f2d5a727ff7e2671deb74cdd
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-04 12:31:42 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.11 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,36 +1,32 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int oneCount = 0;
-        char[] binaryNumber = String.valueOf(Integer.toBinaryString(n)).toCharArray();
+        String nBinary = Integer.toBinaryString(n);
+        int nCount = nBinary.replace("0","").trim().length();
+    
         
-        for(int i=0; i<binaryNumber.length; i++) {
-            if(binaryNumber[i]=='1') oneCount++;
+        for(int next=n+1; next<=1_000_000; next++) {
+            String binary = Integer.toBinaryString(next);
+            int nextCount = binary.replace("0","").trim().length();
+            if(nCount==nextCount) {
+                answer = next;
+                break;
+            }
         }
         
-        answer = calc(n, oneCount);
         return answer;
     }
-    
-    public int calc(int n, int count) {
-        
-        for(int i=n+1; i<=1000000; i++) {
-            int oneCount = 0;
-            char[] binaryNumber = String.valueOf(Integer.toBinaryString(i)).toCharArray();
-            
-            for(int j=0; j<binaryNumber.length; j++) {
-                if(binaryNumber[j]=='1') oneCount++;
-            }
-            
-            if(oneCount == count) return i;
-        }
-        
-        return 0;
-    }
-    
-    
-
-    
 }
+
+// 1. 주어진 n보다 더 크다
+
+// 2. 이진수로 변환했을때 1의 개수가 같다.
+
+// 3. 1,2를 만족하면서 가장 작은 수다.
+
+
+// 1) n부터 반복을 시작한다.
+// 2) 이진수로 변환을 해본다. Integer.toBinaryString() 메소드 사용.
+// 3) 0을 모두 제거한다. replace. trim 사용
+// 4) 길이가 같다면 정답
+// 5) 다르다면 다음반복으로;
