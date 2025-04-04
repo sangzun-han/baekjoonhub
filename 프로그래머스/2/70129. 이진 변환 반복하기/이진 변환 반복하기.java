0d18bbff265d4894fbcbb271ccf9e6722f6d7d91
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-04 11:24:22 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 17.09 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,21 +1,46 @@
-import java.util.*;
-
 class Solution {
+    static int removeZeroCount = 0;
+    static int loopCount = 0;
+    
     public int[] solution(String s) {
         int[] answer = new int[2];
-        int zeroCount = 0;
-        int count = 0;
         
-        while(!s.equals("1")) {
-            int size = s.length();
-            s = s.replaceAll("0","").trim();
-            zeroCount += size - s.length();
-            s = Integer.toBinaryString(s.length());
-            count++;
+        while(true) {
+            // 0을 모두 제거한다.
+            s = removeZero(s);
+            // s의 길이를 c 한다면, 
+            int c = s.length();
+            //길이를 다시 이진법으로 바꾼다
+            s = converBinaryScale(c);
+            loopCount++;
+            if(s.equals("1")) break;
         }
-
-        answer[0] = count;
-        answer[1] = zeroCount;
+        answer[0] = loopCount;
+        answer[1] = removeZeroCount;
+        
         return answer;
     }
+    
+    
+    public String removeZero(String s) {
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)=='0') removeZeroCount++;
+        }
+        
+        return s.replace("0","").trim();
+    }
+    
+    public String converBinaryScale(int c) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(c != 0) {
+            sb.append(c % 2);
+            c /= 2;   
+        }
+        return sb.reverse().toString();
+    }
+    
 }
+
+
+
