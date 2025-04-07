d7e8e6b9a62988e30941ddc9db8382b6cb2551a3
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-07 14:01:33 +0900

    [level 2] Title: 피로도, Time: 31.00 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,25 +1,61 @@
+import java.util.*;
+
+class Dungeon {
+    int 최소필요피로도;
+    int 소모피로도;
+    
+    Dungeon(int 최소필요피로도, int 소모피로도) {
+        this.최소필요피로도 = 최소필요피로도;
+        this.소모피로도 = 소모피로도;
+    }
+}
+
 class Solution {
-    static int[][] Dungeons;
+    static List<Dungeon> dungeonList = new ArrayList<>();
     static boolean[] visited;
-    static int maxDungeons = 0;
+    static int answer;
     
     public int solution(int k, int[][] dungeons) {
-        Dungeons = dungeons;
-        visited = new boolean[Dungeons.length];
-        dfs(k, 0);
+        int K = k;
+        visited = new boolean[dungeons.length];
+        answer = -1;
         
-        return maxDungeons;
+        perm(dungeons, 0, K);
+   
+        return answer;
     }
     
-    private static void dfs(int k, int count) {
-        maxDungeons = Math.max(maxDungeons, count);
+    public void perm(int[][] dungeons, int depth, int K) {
+        if(depth==dungeons.length) {
+            answer = Math.max(calc(K),answer);
+            return;
+        }
         
-        for(int i=0; i<Dungeons.length; i++) {
-            if(!visited[i] && k >= Dungeons[i][0]) {
+        for(int i=0; i<dungeons.length; i++) {
+            if(!visited[i]) {
+                int 최소필요피로도 = dungeons[i][0];
+                int 소모피로도 = dungeons[i][1];
+                dungeonList.add(new Dungeon(최소필요피로도, 소모피로도));
                 visited[i] = true;
-                dfs(k - Dungeons[i][1], count +1);
+                perm(dungeons, depth+1, K);
+                dungeonList.remove(dungeonList.size()-1);
                 visited[i] = false;
             }
         }
+    }
+    
+    public int calc(int k) {
+        int count = 0;
+        for(Dungeon dungeon: dungeonList) {
+            int 최소필요피로도 = dungeon.최소필요피로도;
+            int 소모피로도 = dungeon.소모피로도;
+            
+            if(k >= 최소필요피로도) {
+                k -= 소모피로도;
+                count++;
+            }
+        }
+        
+        return count;
     }
 }
