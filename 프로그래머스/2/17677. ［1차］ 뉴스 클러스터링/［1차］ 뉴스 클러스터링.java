45e15e757bfbd6a020e271296bbff6ad5ed8540d
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-08 15:01:05 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.07 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,57 +1,54 @@
 import java.util.*;
 
 class Solution {
-  
     public int solution(String str1, String str2) {
-        int intersection = 0;
+        int answer = 0;
         int union = 0;
+        int intersection = 0;
         
-        String s1 = str1.toUpperCase();
-        String s2 = str2.toUpperCase();
+        Map<String, Integer> setA = new HashMap<>();
+        Map<String, Integer> setB = new HashMap<>();        
+        Set<String> keys = new HashSet<>();
         
-        Map<String, Integer> s1Map = new HashMap<>();
-        Map<String, Integer> s2Map = new HashMap<>();
-        Set<String> merge = new HashSet<>();
-        
-        for(int i=0; i<s1.length()-1; i++) {
-            String s = s1.charAt(i)+""+s1.charAt(i+1);
-            if(isAlpha(s)) {
-                s1Map.put(s, s1Map.getOrDefault(s, 0) + 1);
+        for(int i=0; i<str1.length()-1; i++) {
+            if(Character.isLetter(str1.charAt(i)) && Character.isLetter(str1.charAt(i+1))) {
+                String s = str1.substring(i,i+2).toUpperCase();
+                setA.put(s, setA.getOrDefault(s, 0) + 1);
             }
         }
         
-        for(int i=0; i<s2.length()-1; i++) {
-            String s = s2.charAt(i)+""+s2.charAt(i+1);
-            if(isAlpha(s)) {
-                s2Map.put(s, s2Map.getOrDefault(s, 0) + 1);
-            }
-        }
-      
-        merge.addAll(s1Map.keySet());
-        merge.addAll(s2Map.keySet());
-        
-        for(String key : merge) {
-            int s1Count = s1Map.getOrDefault(key, 0);
-            int s2Count = s2Map.getOrDefault(key, 0);
-            intersection += Math.min(s1Count, s2Count);
-            union += Math.max(s1Count, s2Count);
-        }
-        
-        if(union == 0) return 65536;
-        
-        double jaccard = (double) intersection / union;
-        return (int) (jaccard * 65536);
-    }
-    
-    private boolean isAlpha(String s) {
-        char[] c = s.toCharArray();
-        
-        for(int i=0; i<c.length; i++) {
-            if(!Character.isAlphabetic(c[i])) {
-                return false;
+        for(int i=0; i<str2.length()-1; i++) {
+            if(Character.isLetter(str2.charAt(i)) && Character.isLetter(str2.charAt(i+1))) {
+                String s = str2.substring(i,i+2).toUpperCase();
+                setB.put(s, setB.getOrDefault(s, 0) + 1);
             }
         }
         
-        return true;
+        for(String key: setA.keySet()) {
+            keys.add(key);
+        }
+        
+        for(String key: setB.keySet()) {
+            keys.add(key);
+        }
+        
+        
+        for(String key: keys) {
+            int aCount = setA.getOrDefault(key, 0);
+            int bCount = setB.getOrDefault(key, 0);
+            
+            union += Math.max(aCount,bCount);
+            
+            if(aCount==0 || bCount==0) continue;
+            intersection += Math.min(aCount, bCount);
+        }
+        
+        if(union==0) return 65536;
+        else {
+            double d = ((double)intersection/(double)union);
+            answer = (int)(d*65536);
+        }
+        
+        return answer;
     }
 }
