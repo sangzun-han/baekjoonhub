68f62acb16877af3f9d7234cf2a4147b4b07dbdd
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-08 11:52:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 174.55 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,27 +3,36 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Set<Integer> set = new HashSet<>(); 
-        Map<Integer, Integer> map = new HashMap<>();
-    
-        for(int top: topping) {
-            map.put(top, map.getOrDefault(top, 0) + 1);
+        int size = topping.length;
+        Set<Integer> left = new HashSet<>();
+        Set<Integer> right = new HashSet<>();
+        int[] leftKind = new int[size];
+        int[] rightKind = new int[size];
+        
+        for(int i=0; i<size; i++) {
+            left.add(topping[i]);
+            leftKind[i] = left.size();    
         }
         
-        for(int top: topping) {
-            set.add(top);
-            map.put(top, map.getOrDefault(top, 0) -1);
-            
-            if(map.get(top) == 0) {
-                map.remove(top);
-            }
-            
-            if(set.size() == map.size()) {
-                answer++;
-            }
+        for(int i=size-1; i>=0; i--) {
+            right.add(topping[i]);
+            rightKind[i] = right.size();    
         }
         
-    
+        for(int i=0; i<size-1; i++) {
+            if(leftKind[i]==rightKind[i+1]) answer++;
+        }
         return answer;
     }
 }
+
+// 이중반복 x -> 시간초과
+// 그리디? 모든 토핑 종류 set에저장, -> 토핑의 수를 알 수 있음 그래도안됨
+// 자료구조로 set이용?
+// 공평하게 자르는 방법의 수니까 결국 완탐 돌긴해야함
+// 어떻게 자르냐?
+// 1개 2개 3개 4개 n개
+// map
+// 누적합? 0부터 I까지갯수 i부터 n까지 개수 dp?
+// left[i] : 0~i까지의 토핑 갯수
+// right[i] : i~n-1까지 토핑의 개수
