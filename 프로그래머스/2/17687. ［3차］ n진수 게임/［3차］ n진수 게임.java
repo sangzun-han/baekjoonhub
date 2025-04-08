030a5ccc43d2fefd3917d48319dda0a066637d8a
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-08 17:33:14 +0900

    [level 2] Title: [3차] n진수 게임, Time: 221.50 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -2,50 +2,72 @@ import java.util.*;
 
 class Solution {
     static List<String> list = new ArrayList<>();
-    StringBuilder sb = new StringBuilder();
+    static List<String> splitList = new ArrayList<>();
+    
+    static StringBuilder sb = new StringBuilder();
+
     public String solution(int n, int t, int m, int p) {
-        
-        String answer = "";
         int size = t*m+1;
         int number = 0;
+        int start = p - 1;
+        String answer = "";
+        
         while(list.size() < size) {
-            nbase(n, number);
+            n진법(n, number);
             number++;
         }
         
-        for(int i=0; i<t; i++) {
-            sb.append(list.get(p-1 + (i *m)));
+        for(int i=0; i<list.size(); i++) {
+            String[] stringSplit = list.get(i).split("");
+            for(int j=0; j<stringSplit.length; j++) {
+                splitList.add(stringSplit[j]);   
+            }
         }
+        
+        for(int i=0; i<t; i++) {
+            sb.append(splitList.get(start+(m*i)));
+        }
+            
         return sb.toString();
     }
     
-    public void nbase(int n, int number) {
-        if(number == 0) {
+    public void n진법(int n, int number) {
+        if(number==0) {
             list.add("0");
             return;
         }
-        
         while(number > 0) {
             int remain = number % n;
             String convert = "";
             switch(remain) {
-                case 10: convert = "A"; break;
-                case 11: convert = "B"; break;
-                case 12: convert = "C"; break;
-                case 13: convert = "D"; break;
-                case 14: convert = "E"; break;
-                case 15: convert = "F"; break;
-                default: convert = String.valueOf(remain);
+                case 10:
+                    convert = "A";
+                    break;
+                case 11:
+                    convert = "B";
+                    break;
+                case 12:
+                    convert = "C";
+                    break;
+                case 13:
+                    convert = "D";
+                    break;
+                case 14:
+                    convert = "E";
+                    break;
+                case 15:
+                    convert = "F";
+                    break;
+                default:
+                    convert = String.valueOf(remain);
+                    break;
             }
             sb.append(convert);
             number = number / n;
         }
-        sb.reverse();
         
-        for(int i=0; i<sb.length(); i++) {
-            list.add(String.valueOf(sb.charAt(i)));
-        }
-        
+        list.add(sb.reverse().toString());
         sb.setLength(0);
     }
 }
+
