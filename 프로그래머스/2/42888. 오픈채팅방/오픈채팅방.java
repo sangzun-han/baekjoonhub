05983366be61ecff06868284b67e7848c9a81e3d
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-11 17:11:11 +0900

    [level 2] Title: 오픈채팅방, Time: 138.24 ms, Memory: 175 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,42 +3,47 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         StringBuilder sb = new StringBuilder();
-        
+        List<String> answerList = new ArrayList<>();
         Map<String, String> map = new HashMap<>();
-        List<String> list = new ArrayList<>();
         
-        for(int i=0; i<record.length ;i++) {
-            String rec = record[i];
-            String[] recordSplit = rec.split(" ");
-            String type = recordSplit[0];
-            if(type.equals("Enter") || type.equals("Change")) {
-                String uid = recordSplit[1];
-                String nickname = recordSplit[2];    
-                map.put(uid, nickname);
+        for(int i=0; i<record.length; i++) {
+            String[] s = record[i].split(" ");
+            String command = s[0];
+            if(command.equals("Enter")) {
+                map.put(s[1], s[2]);
+            } else if(command.equals("Change")) {
+                map.put(s[1], s[2]);
             }
         }
         
         for(int i=0; i<record.length; i++) {
-            String rec = record[i];
-            String[] recordSplit = rec.split(" ");
-            String type = recordSplit[0];
-            String uid = recordSplit[1];
-            String nickname = map.get(uid);
-            if(type.equals("Enter")) {
-                sb.append(nickname).append("님이").append(" ").append("들어왔습니다.");
-                list.add(sb.toString());
-            } else if(type.equals("Leave")) {
-                sb.append(nickname).append("님이").append(" ").append("나갔습니다.");
-                list.add(sb.toString());
+            String[] s = record[i].split(" ");
+            String command = s[0];
+            String uid = s[1];
+
+            if(command.equals("Enter")) {
+                sb
+                    .append(map.get(uid))
+                    .append("님이 ")
+                    .append("들어왔습니다.");
+            } else if(command.equals("Leave")) {
+                sb
+                    .append(map.get(uid))
+                    .append("님이 ")
+                    .append("나갔습니다.");
+            } else {
+                continue;
             }
+            answerList.add(sb.toString());
             sb.setLength(0);
         }
         
-        String[] answer = new String[list.size()];
-        for(int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        String[] answer = new String[answerList.size()];
         
+        for(int i=0; i<answerList.size(); i++) {
+            answer[i] = answerList.get(i);
+        }
+
         return answer;
     }
 }
