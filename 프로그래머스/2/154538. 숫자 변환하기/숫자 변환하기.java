6752cbbedcbed5484f629ad924819212ed6d8592
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-11 15:02:10 +0900

    [level 2] Title: 숫자 변환하기, Time: 47.11 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Point {
     int num;
     int count;
@@ -11,32 +10,28 @@ class Point {
 }
 
 class Solution {
-    static Map<Integer, Integer> visited = new HashMap<>();
-    
     public int solution(int x, int y, int n) {
-        return bfs(x,y,n);
-    }
-    
-    public int bfs(int x, int y, int n) {
-        if(x==y) return 0;
-        
+        int answer = -1;
         Queue<Point> queue = new ArrayDeque<>();
-        visited.put(x, 0);
+        boolean[] visited = new boolean[y+1];
+        
         queue.offer(new Point(x, 0));
+        visited[x] = true;
         
         while(!queue.isEmpty()) {
             Point p = queue.poll();
+            if(p.num == y) return p.count;
+            
             int[] nextNums = {p.num + n, p.num * 2, p.num * 3};
             
-            for(int next: nextNums) {
-                if(next > y) continue;
-                if (visited.containsKey(next) && visited.get(next) <= p.count + 1) continue;
-                if(next==y) return p.count + 1;
-                queue.offer(new Point(next, p.count + 1));
-                visited.put(next, p.count + 1);
+            for (int next : nextNums) {
+                if (next <= y && !visited[next]) {
+                    visited[next] = true;
+                    queue.offer(new Point(next, p.count + 1));
+                }
             }
         }
         
-        return -1;
+        return answer;
     }
 }
