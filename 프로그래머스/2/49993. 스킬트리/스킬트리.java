e6ea0041afe6aab51d3f84a49534da7b9030dc72
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-11 10:30:22 +0900

    [level 2] Title: 스킬트리, Time: 0.24 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,27 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
+        StringBuilder sb = new StringBuilder();
         int answer = 0;
-        
         
         for(int i=0; i<skill_trees.length; i++) {
             String skill_tree = skill_trees[i];
             
-            for(char c: skill_tree.toCharArray()) {
-                if(!skill.contains(String.valueOf(c))) {
-                    skill_tree = skill_tree.replace(String.valueOf(c),"");
+            for(int j=0; j<skill_tree.length(); j++) {
+                String s = String.valueOf(skill_tree.charAt(j));
+                if(skill.contains(s)) {
+                    sb.append(s);
                 }
             }
-            skill_trees[i] = skill_tree;
+            
+            if(skill.startsWith(sb.toString())) answer++;
+            sb.setLength(0);
         }
-        
-        for(int i=0; i<skill_trees.length; i++) {
-            if(skill.startsWith(skill_trees[i])) answer++;
-        }
-        
-       
         return answer;
     }
 }
 
+
+// skill_trees에서 skill에 있는 문자로만 표현 -> skill에 없다면 빈걸로 만들든 sb쓰든
+// 이후에 startsWith 쓰면될듯?
