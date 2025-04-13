4ed309b072202ed0f3be393fe98478126f8ad162
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-13 21:36:15 +0900

    [level 2] Title: 가장 큰 수, Time: 2823.19 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,26 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        StringBuilder sb = new StringBuilder();
-        
+        String answer = "";
         List<String> list = new ArrayList<>();
-        
+    
         for(int i=0; i<numbers.length; i++) {
             list.add(String.valueOf(numbers[i]));
         }
         
-        Collections.sort(list, new Comparator<String>(){
-            public int compare(String s1, String s2) {                
-                return (s2+s1).compareTo(s1+s2);
-            }   
-        });
+        list.sort((a,b) -> (b+a).compareTo(a+b));
         
         if(list.get(0).equals("0")) return "0";
         
         for(int i=0; i<list.size(); i++) {
-            sb.append(list.get(i));
+            answer += list.get(i);
         }
-            
-        return sb.toString();
+     
+
+        return answer;
     }
 }
