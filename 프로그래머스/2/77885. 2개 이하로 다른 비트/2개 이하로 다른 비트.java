f758891470811a5a04abc6421bbc3c4587f11925
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-13 20:44:14 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 30.48 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -3,22 +3,20 @@ import java.util.*;
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
-        // XOR 연산 -> 1의 개수가 2개 이하면 되는듯? 문제는 범위 : 10^15
         for(int i=0; i<numbers.length; i++) {
             long number = numbers[i];
             long next = number;
+            int count = 0;
             if(number%2==0) {
                 answer[i] = number + 1;
             } else {
-                int count = 0;
-                while(next % 2 == 1) {
+                while(next%2==1) {
                     count++;
                     next /= 2;
                 }
-                answer[i] = number + (long) Math.pow(2, count-1);
+                answer[i] = number + (long)Math.pow(2, count-1);
             }
         }
-        
         return answer;
     }
 }
