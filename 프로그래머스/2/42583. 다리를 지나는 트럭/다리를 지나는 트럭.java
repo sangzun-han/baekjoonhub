6303c10361b16ee0386eb8c09308bb0fa68f3c25
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-14 16:10:39 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 23.64 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -6,38 +6,42 @@ class Truck {
     
     Truck(int weight, int time) {
         this.weight = weight;
-        this.time = time; // 다리 다 건너는 시간
+        this.time = time;
     }
 }
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new ArrayDeque<>();
+        int answer = 0;
+        Queue<Integer> waiting = new ArrayDeque<>();
         Queue<Truck> bridge = new ArrayDeque<>();
         
         for(int i=0; i<truck_weights.length; i++) {
-            queue.offer(truck_weights[i]);
+            waiting.offer(truck_weights[i]);
         }
         
         int time = 0;
-        int bridge_weight = 0;
+        int totalWeight = 0;
         
-        while(!queue.isEmpty() || !bridge.isEmpty()) {
+        while(!waiting.isEmpty() || !bridge.isEmpty()) {
             time++;
-            // 1. 다리를 지나간 트럭 제거
-            if(!bridge.isEmpty() && bridge.peek().time == time) {
-                Truck truck =bridge.poll();
-                bridge_weight -= truck.weight;
-            }
             
-            // 2. 다리에 올라갈 트럭 적재
-            if(!queue.isEmpty() && bridge_weight + queue.peek() <= weight) {
-                int w = queue.poll();
-                bridge.offer(new Truck(w, time + bridge_length));
-                bridge_weight += w;
+            // 트럭 빼기
+            if(!bridge.isEmpty() && time - bridge.peek().time >= bridge_length) {
+                Truck truck = bridge.poll();
+                totalWeight -= truck.weight;   
             }
-
+            // 새로운 트럭 투입
+           if(!waiting.isEmpty() && totalWeight + waiting.peek() <= weight) {
+               int truckWeight = waiting.poll();
+               totalWeight += truckWeight;
+               bridge.offer(new Truck(truckWeight, time));
+           }
         }
+       
         return time;
     }
 }
+
+
+
