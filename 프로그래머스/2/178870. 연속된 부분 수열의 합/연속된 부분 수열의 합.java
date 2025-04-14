5186cb1f1646f9dfb25f2a6fbbe76ae86a5f6ecc
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-14 19:33:55 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 10.37 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -3,36 +3,41 @@ class Solution {
         int[] answer = new int[2];
         int left = 0;
         int right = 0;
-        int minLength = Integer.MAX_VALUE;
-        int sum = sequence[0];
+        int diff = Integer.MAX_VALUE;
+        int sum = sequence[left];
         
         while(right < sequence.length) {
             if(sum < k) {
-                if(right < sequence.length - 1) {
-                    right++;
-                    sum += sequence[right];
-                } else {
-                    break;
-                }
-
-            }
-            else if (sum > k) {
-                sum -= sequence[left];
-                left++;
-                if(left > right) right = left;
-            }
-            else if(sum==k) {
-                if(right - left < minLength) {
-                    minLength = right - left;
+                if(++right < sequence.length)
+                sum += sequence[right];
+            } else if (sum > k) {
+                sum -= sequence[left++];
+            } else if (sum == k) {
+                if(right-left < diff) {
+                    diff = right - left;
                     answer[0] = left;
                     answer[1] = right;
                 }
-                sum -= sequence[left];
-                left++;
-                if(left > right) right = left;
                 
+                sum -= sequence[left++];
+            }
+            
+            if(left > right) {
+                int temp = right;
+                right = left;
+                left = temp;
             }
         }
+        
         return answer;
     }
 }
+
+// 투포인터
+// target이 k
+// sequence  0부터 시작,
+// 만약 타겟이면 가장 짧은 길이 인지 체크 짧다면 체크;
+// 만약 타겟보다 작으면 left 증가
+// 만약 타겟보다 크다면 right 증가
+// right < left 보다 커지는 경우가 있다면 교체 -> 이런 경우 존재하는지 모르겠음
+// right가 n까지 도달했을때 종료
