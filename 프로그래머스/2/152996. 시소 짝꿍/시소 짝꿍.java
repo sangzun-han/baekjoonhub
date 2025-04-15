d77796349c110099d2329e4a3e4d0aa88ed11d50
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-15 10:44:14 +0900

    [level 2] Title: 시소 짝꿍, Time: 174.37 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -8,17 +8,15 @@ class Solution {
         
         for(int i=0; i<weights.length; i++) {
             double weight = (double) weights[i];
-            answer += map.getOrDefault(weight, 0) 
-                + map.getOrDefault((weight * 2) / 3, 0)
-                + map.getOrDefault((weight * 3) / 2, 0)
-                + map.getOrDefault((weight * 3) / 4, 0)
-                + map.getOrDefault((weight * 4) / 3, 0)
-                + map.getOrDefault(weight / 2, 0)
-                + map.getOrDefault(weight * 2, 0);
             
-            map.put(weight, map.getOrDefault(weight,0) + 1);
-            
+            answer += map.getOrDefault(weight, 0)
+                + map.getOrDefault(weight * 2 / 3.0, 0)
+                + map.getOrDefault(weight * 1 / 2.0, 0)
+                + map.getOrDefault(weight * 3 / 4.0, 0);
+                
+            map.put(weight, map.getOrDefault(weight, 0) + 1);
         }
+        
         return answer;
     }
 }
