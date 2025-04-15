1cb58c2185564f7b6e57c430dd6c962e3f16eb28
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-15 17:48:35 +0900

    [level 2] Title: 미로 탈출, Time: 6.31 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,85 +1,96 @@
 import java.util.*;
 
 class Point {
-    int x;
-    int y;
-    int time;
+    int row;
+    int col;
+    int count;
     
-    Point(int x, int y, int time) {
-        this.x = x;
-        this.y = y;
-        this.time = time;
+    Point(int row, int col, int count) {
+        this.row = row;
+        this.col = col;
+        this.count = count;
     }
 }
 
 class Solution {
-    static int N;
-    static int M;
-    static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};
     static char[][] map;
+    static int n,m;
+    static int[] dr = {-1,0,1,0};
+    static int[] dc = {0,1,0,-1};
+    
     public int solution(String[] maps) {
         int answer = 0;
-        N = maps.length;
-        M = maps[0].length();
-        map = new char[N][M];
+        int startR = 0;
+        int startC = 0;
+        int leverR = 0;
+        int leverC = 0;
+        int exitR = 0;
+        int exitC = 0;
         
-        int startX = 0, startY = 0, leverX = 0, leverY = 0, endX = 0, endY = 0;
-        for(int i=0; i<N; i++) {
-            char[] c = maps[i].toCharArray();
-            for(int j=0; j<M; j++) {
-                map[i][j] = c[j];
-                if(c[j]=='S') {
-                    startX = i;
-                    startY = j;
-                } else if(c[j]=='L') {
-                    leverX = i;
-                    leverY = j;
-                } else if(c[j]=='E') {
-                    endX = i;
-                    endY = j;
+        n = maps.length;
+        m = maps[0].length();
+        map = new char[n][m];
+        
+        for(int i=0; i<n; i++) {
+            map[i] = maps[i].toCharArray();
+        }
+        
+        for(int i=0; i<n; i++)  {
+            for(int j=0; j<m; j++) {
+                if(map[i][j]=='S') {
+                    startR = i;
+                    startC = j;
                 }
-            }
+                
+                if(map[i][j]=='L') {
+                    leverR = i;
+                    leverC = j;
+                }
+                
+                if(map[i][j]=='E') {
+                    exitR = i;
+                    exitC = j;
+                }
+            }    
         }
         
-        int minLever = bfs(startX, startY, leverX, leverY);
-        int minExit = bfs(leverX, leverY, endX, endY);
+        int count = bfs(startR, startC, leverR, leverC);
+        if(count == -1) return -1;
+        answer += count;
         
-        if(minLever==0|| minExit==0) {
-            return -1;
-        } else {
-            answer = minLever + minExit;
-        }
+        count = bfs(leverR, leverC, exitR, exitC);
+        if(count == -1) return -1;
+        answer += count;
         
         return answer;
     }
     
-    public static int bfs(int x1, int y1, int x2, int y2) {
+    public int bfs(int row1, int col1, int row2, int col2) {
         Queue<Point> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[N][M];
-        visited[x1][y1] = true;
-        queue.offer(new Point(x1,y1,0));
-
+        boolean[][] visited = new boolean[n][m];
+        
+        visited[row1][col1] = true;
+        queue.offer(new Point(row1, col1, 0));
+        
         while(!queue.isEmpty()) {
+            
             Point p = queue.poll();
             
+            if(p.row==row2 && p.col==col2) return p.count;
+            
             for(int d=0; d<4; d++) {
-                int nx = p.x + dx[d];
-                int ny = p.y + dy[d];
-                int time = p.time;
-                if(nx==x2 && ny==y2) {
-                    return time+1;
-                }
-                if(nx>=0 && nx<N && ny>=0 && ny<M && !visited[nx][ny]) {
-                    if(map[nx][ny] != 'X') {
-                        visited[nx][ny] = true;
-                        queue.offer(new Point(nx, ny, time+1));     
-                    }           
+                int nr = p.row + dr[d];
+                int nc = p.col + dc[d];
+                int nCount = p.count;
+                
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && map[nr][nc] != 'X') {
+                    queue.offer(new Point(nr,nc, nCount+1));
+                    visited[nr][nc] = true;
                 }
             }
         }
         
-        return 0;
+        return -1;
+        
     }
-    
 }
