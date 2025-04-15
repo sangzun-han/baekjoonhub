14a54e728988374276fc16699e726a215ff3060f
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-15 14:50:00 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 22.36 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,63 +1,9 @@
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        
-        int agcd = arrayA[0];
-        int bgcd = arrayB[0];
-        
-        for(int i=1; i<arrayA.length; i++) {
-            agcd = GCD(agcd, arrayA[i]);
-        }
-        
-        for(int i=1; i<arrayB.length; i++) {
-            bgcd = GCD(bgcd, arrayB[i]);
-        }
-        
-        boolean adivide = true;
-        boolean bdivide = true;
-        
-        boolean cdivide = true;
-        boolean ddivide = true;
-        
-        // 철수가 가진 카드를 나눌수 있는지 없는지 판단
-        // 철수꺼 나눌수 있어야함
-        for(int i=0; i<arrayA.length; i++) {
-            if(arrayA[i] % agcd != 0) {
-                adivide = false;
-                break;
-            }
-        }
-        
-        // 영희꺼는 나눌수없어야함
-        for(int i=0; i<arrayB.length; i++) {
-            if(arrayB[i] % agcd == 0) {
-                bdivide = false;        
-                break;
-            }
-        }
-        
-        for(int i=0; i<arrayA.length; i++) {
-            if(arrayA[i] % bgcd == 0) {
-                cdivide = false;
-                break;
-            }
-        }
-        
-        // 영희꺼는 나눌수없어야함
-        for(int i=0; i<arrayB.length; i++) {
-            if(arrayB[i] % bgcd != 0) {
-                ddivide = false;        
-                break;
-            }
-        }
-        
-        if(adivide && bdivide) {
-            answer = Math.max(agcd,answer);
-        }  
-        if(cdivide && ddivide) {
-            answer = Math.max(bgcd,answer);
-        }        
-        
+        int a = findDivision(arrayA, arrayB);
+        int b = findDivision(arrayB, arrayA);
+        answer = Math.max(a,b);
         return answer;
     }
     
@@ -69,6 +15,21 @@ class Solution {
         }
         
         return a;
+    }
+    
+    public int findDivision(int[] arrayA, int[] arrayB) {
+        int gcd = arrayA[0];
         
+        for(int i=1; i<arrayA.length; i++) {
+            gcd = GCD(gcd, arrayA[i]);
+        }
+        
+        for(int i=0; i<arrayB.length; i++) {
+            if(arrayB[i] % gcd == 0) {
+                return 0;
+            }
+        }
+        
+        return gcd;
     }
 }
