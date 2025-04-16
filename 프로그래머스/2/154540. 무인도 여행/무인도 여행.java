3b4ced7a8a128f549e17e9635b386b6c67c698ea
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-16 15:09:26 +0900

    [level 2] Title: 무인도 여행, Time: 10.28 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -13,65 +13,62 @@ class Point {
 class Solution {
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
-    static boolean[][] visited;
     static char[][] map;
-    static int row;
-    static int col;
+    static int n,m;
+    static boolean[][] visited;
     
     public int[] solution(String[] maps) {
-        
         List<Integer> list = new ArrayList<>();
-        row = maps.length;
-        col = maps[0].length();
         
-        map = new char[row][col];
-        visited = new boolean[row][col];
+        n = maps.length;
+        m = maps[0].length();
+        map = new char[n][m];
+        visited = new boolean[n][m];
         
-        for(int i=0; i<row; i++) {
-            map[i] = maps[i].toCharArray();
+        for(int i=0; i<n; i++) {
+            map[i] = maps[i].toCharArray();    
         }
         
-        for(int i=0; i<row; i++) {
-            for(int j=0; j<col; j++) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 if(map[i][j]!='X' && !visited[i][j]) {
-                    int eat = bfs(i,j);
-                    list.add(eat);
+                    int food = bfs(i,j);
+                    if(food != 0) list.add(food);
                 }
             }
         }
+        
         if(list.size()==0) return new int[]{-1};
-        
-        Collections.sort(list);
-        
         int[] answer = new int[list.size()];
-        
+        Collections.sort(list);
         for(int i=0; i<list.size(); i++) {
             answer[i] = list.get(i);
         }
+        
         return answer;
     }
     
     public int bfs(int r, int c) {
-        int eat = 0;
+        int food = Integer.parseInt(String.valueOf(map[r][c]));
         Queue<Point> queue = new ArrayDeque<>();
         queue.offer(new Point(r,c));
         visited[r][c] = true;
         
         while(!queue.isEmpty()) {
             Point p = queue.poll();
-            eat += map[p.r][p.c] - '0';
             
             for(int d=0; d<4; d++) {
                 int nr = p.r + dr[d];
                 int nc = p.c + dc[d];
                 
-                if(nr>=0 && nr<row && nc>=0 && nc<col && !visited[nr][nc] && map[nr][nc]!='X') {
-                    queue.offer(new Point(nr,nc));
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && map[nr][nc] != 'X') {
+                    food += Integer.parseInt(String.valueOf(map[nr][nc]));
+                    queue.offer(new Point(nr, nc));
                     visited[nr][nc] = true;
                 }
             }
         }
         
-        return eat;
+        return food;
     }
 }
