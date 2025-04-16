d851caefb86c2b5c294dcea7896beb4a8f6f7267
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-16 09:22:56 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.33 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,21 +1,40 @@
+import java.util.*;
+
 class Solution {
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
-        int N = n;
         
-        while(N>0) {
-            if(N%3!=0) {
-                sb.append(N%3);
-                N = N / 3;
-            } else {
-                sb.append(4);
-                N = N / 3 - 1;
+        Stack<Integer> stack = new Stack<>();
+        
+        while(n!=0) {
+            int remain = n % 3;
+            if(remain != 0) {
+                stack.push(remain);
+                n /= 3;
+            } else if(remain == 0) {
+                stack.push(4);
+                n = (n / 3) - 1;
             }
         }
-        return sb.reverse().toString();
+        
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        
+        return sb.toString();
     }
-   
 }
 
+// 3진법
+// 1 % 3 = 1
+// 2 % 3 = 2;
+// 3 % 3 = 0 -> 4
 
+// 4 % 3 = 1 -> 1 % 3 = 1 , 11
+// 5 %3 = 2 -> 2  1 % 3 = 1
+// 6 % 3 = 0 -> 4  2 % 3 =  2  24
+// 7 % 3 = 1 2 % 3 = 2  21
+// 8 % 3 = 2 2 % 3 = 2 22
+// 9 % 3 = 0 -> 4 2 % 3 = 0 
 
+// %3 == 0 이라면 다음 나눌때 /3 -1을 한다. 이유는 모름 그런규칙인데?
