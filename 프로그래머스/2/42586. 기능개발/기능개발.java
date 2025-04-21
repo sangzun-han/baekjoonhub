49892db56e2521a91f20f052fed1a14bab22f7dc
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-21 09:52:37 +0900

    [level 2] Title: 기능개발, Time: 0.60 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,35 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
         List<Integer> answerList = new ArrayList<>();
         
         for(int i=0; i<speeds.length; i++) {
-            int remain = 100 - progresses[i];
-            int day = (int)Math.ceil((double)remain/speeds[i]);
-            list.add(day);
+            int speed = speeds[i];
+            int progress = progresses[i];
+            int day = (int)Math.ceil((double)((100 - progress) / (double)speed));
+            queue.offer(day);
         }
         
-        int current = list.get(0);
-        int count = 1;
-        for(int i=1; i<list.size(); i++) {
-    
-            if(current >= list.get(i)) {
+        while(!queue.isEmpty()) {
+            int day = queue.poll();
+            int count = 1;
+            
+            while(!queue.isEmpty() && day >= queue.peek()) {
+                queue.poll();
                 count++;
-            } else {
-                answerList.add(count);
-                count = 1;
-                current = list.get(i);
             }
+            answerList.add(count);
         }
-        answerList.add(count);
+        
         int[] answer = new int[answerList.size()];
+        
         for(int i=0; i<answerList.size(); i++) {
             answer[i] = answerList.get(i);
         }
-
         return answer;
     }
 }
-
-
