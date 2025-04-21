1fcaf22501dc5a88ff8cd88b0644a1335df27634
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-21 21:53:54 +0900

    [level 2] Title: 비밀 코드 해독, Time: 32.31 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,36 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static List<Integer> number = new ArrayList<>();
+    static List<Integer> list = new ArrayList<>();
     static int answer;
     public int solution(int n, int[][] q, int[] ans) {
         answer = 0;
-        comb(0,1,n,q,ans);
+        comb(n, q, ans, 0, 1);    
         return answer;
     }
     
-    public void comb(int count, int index, int n, int[][] q, int[] ans) {
-        if(count==5) {
-            if(checkMath(q,ans)) answer++;
+    public void comb(int n, int[][] q, int[] ans, int depth, int index) {
+        if(depth==5) {
+            if(calc(q,ans)) answer++;
             return;
         }
         
         for(int i=index; i<=n; i++) {
-            number.add(i);
-            comb(count + 1, i+1, n, q, ans);
-            number.remove(number.size() -1);
+            list.add(i);
+            comb(n, q, ans, depth+1, i+1);
+            list.remove(list.size() - 1);
         }
     }
     
-    public boolean checkMath(int[][] q, int[] ans) {
+    public boolean calc(int[][] q, int[] ans) {
         for(int i=0; i<q.length; i++) {
             int count = 0;
             for(int j=0; j<5; j++) {
-                if(number.contains(q[i][j])) {
+                if(list.contains(q[i][j])) {
                     count++;
                 }
             }
-            if(count!=ans[i]) return false;
+            
+            if(count != ans[i]) return false;
+            
         }
         
         return true;
