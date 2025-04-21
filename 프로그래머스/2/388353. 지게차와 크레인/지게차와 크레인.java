72e09f98d8b582d9bd1efce93b6cfbedf438ce25
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-21 15:03:16 +0900

    [level 2] Title: 지게차와 크레인, Time: 101.32 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 class Point {
-    int x;
-    int y;
+    int r;
+    int c;
     
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    Point(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
 
 class Solution {
-    static char[][] map;
+    static char[][] storages;
     static int n,m;
+    static List<Point> deleteList = new ArrayList<>();
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
     
@@ -21,79 +22,81 @@ class Solution {
         n = storage.length;
         m = storage[0].length();
         
-        map = new char[n][m];
+        storages = new char[n][m];
         
         for(int i=0; i<n; i++) {
-            map[i] = storage[i].toCharArray();
+            storages[i] = storage[i].toCharArray();
         }
         
-        for(int i=0; i<requests.length; i++) {
-            char c = requests[i].charAt(0);
-            if(requests[i].length()==1) removeOne(c);
-            else removeAll(c);
-        }
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(map[i][j] != '.') answer++;
+        for(int k=0; k<requests.length; k++) {
+            String request = requests[k];
+            // bfs돌던가, 외부와 연결됐던가
+            // 삭제할 수 있는거 다 모아서 한꺼번에 삭제.
+
+            if(request.length()==1) {
+                for(int i=0; i<n; i++) {
+                    for(int j=0; j<m; j++) {    
+                        if(request.charAt(0) == storages[i][j]) {
+                            if(canEscape(i,j) ||isOut(i, j)) {
+                                deleteList.add(new Point(i,j));
+                            }
+                        }
+                    }
+                }
+                
+                for(Point p: deleteList) {
+                    storages[p.r][p.c] = '.';
+                }
+                
+                deleteList.clear();
             }
-        }
-        
-        return answer;
-    }
-    
-    public void removeOne(char target) {
-        List<Point> removePoint = new ArrayList<>();
-    
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(map[i][j]==target && (isOutside(i,j) || canEscape(i,j))) {
-                    removePoint.add(new Point(i,j));
+            else {
+                for(int i=0; i<n; i++) {
+                    for(int j=0; j<m; j++) {
+                        if(storages[i][j]==request.charAt(0)) {
+                            storages[i][j] = '.';
+                        }
+                    }
                 }
             }
         }
         
-        for(Point p: removePoint) {
-            map[p.x][p.y] = '.';
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(storages[i][j]!='.') answer++;
+            }
         }
+        
+        
+        return answer;
     }
     
-    public boolean isOutside(int r, int c) {
-        if(r==0 || r==n-1 || c==0 || c==m-1) return true;
-        return false;
+    public boolean isOut(int r, int c) {
+        return (r==n-1 || r==0 || c==m-1 || c==0);
     }
     
     public boolean canEscape(int r, int c) {
         Queue<Point> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-        
         queue.offer(new Point(r,c));
         visited[r][c] = true;
         
         while(!queue.isEmpty()) {
             Point p = queue.poll();
             
+            if(isOut(p.r, p.c)) return true;
+            
             for(int d=0; d<4; d++) {
-                int nr = p.x + dr[d];
-                int nc = p.y + dc[d];
+                int nr = p.r + dr[d];
+                int nc = p.c + dc[d];
                 
-                if(nr<0 || nr>=n || nc<0 || nc>=m) return true;
-                
-                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && map[nr][nc]=='.') {
-                    queue.offer(new Point(nr,nc));
+                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && storages[nr][nc] == '.') {
+                    queue.offer(new Point(nr, nc));
                     visited[nr][nc] = true;
                 }
             }
         }
         
         return false;
-    }
-    
-    public void removeAll(char target) {
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(map[i][j]==target) map[i][j]='.';
-            }
-        }
     }
 }
