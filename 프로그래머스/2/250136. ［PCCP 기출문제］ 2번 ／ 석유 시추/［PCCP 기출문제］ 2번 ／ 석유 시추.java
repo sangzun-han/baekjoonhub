9ae70eea098b1a7694eb1961e4fbea709b7ee470
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-04-25 11:24:21 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 82.53 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,72 +1,67 @@
 import java.util.*;
 
 class Point {
-    int x;
-    int y;
+    int r;
+    int c;
     
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    Point(int r, int c) {
+        this.r = r;
+        this.c = c;
     }
 }
 
 class Solution {
-    static boolean[][] visited;
     static int n,m;
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
-    
     static Map<Integer, Integer> map = new HashMap<>();
     
     public int solution(int[][] land) {
         int answer = 0;
         n = land.length;
         m = land[0].length;
-        visited = new boolean[n][m];
         
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
-                if(land[i][j]==1 && !visited[i][j]) {
-                    bfs(i,j,land);
-                }
+                if(land[i][j]==1) {
+                    bfs(i, j, land);
+                }        
             }
         }
         
-        for(int key: map.keySet()) {
-            answer = Math.max(map.get(key), answer);
+        for(int count: map.values()) {
+            answer = Math.max(answer, count);
         }
         
         return answer;
     }
     
     public void bfs(int r, int c, int[][] land) {
-        int count = 0;
+        int count = 1;
+        Set<Integer> set = new HashSet<>();
         Queue<Point> queue = new ArrayDeque<>();
-        List<Integer> cols = new ArrayList<>();
-        
-        visited[r][c] = true;
         queue.offer(new Point(r,c));
+        land[r][c] = 0;
         
         while(!queue.isEmpty()) {
-            Point p  = queue.poll();
-            count++;
-            
-            if(!cols.contains(p.y)) cols.add(p.y);
+            Point p = queue.poll();
+            set.add(p.c);
             
             for(int d=0; d<4; d++) {
-                int nr = p.x + dr[d];
-                int nc = p.y + dc[d];
+                int nr = p.r + dr[d];
+                int nc = p.c + dc[d];
                 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && land[nr][nc]==1) {
-                    queue.offer(new Point(nr,nc));
-                    visited[nr][nc] = true;
+                if(nr>=0 && nr<n && nc>=0 && nc<m && land[nr][nc]==1) {
+                    queue.offer(new Point(nr,nc));    
+                    land[nr][nc] = 0;
+                    count++;
                 }
             }
         }
         
-        for(int col: cols) {
-            map.put(col, map.getOrDefault(col, 0) + count);     
+        for(int col: set) {
+            map.put(col, map.getOrDefault(col, 0) + count);
         }
-       
+        return;
     }
 }
