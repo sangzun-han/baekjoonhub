ec714f7bdcfe932ebde39c8eeb166876a483e404
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-07-10 17:35:49 +0900

    [Gold IV] Title: 인구 이동, Time: 720 ms, Memory: 299152 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,10 +1,11 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Point {
     int r;
     int c;
-    Point(int r, int c) {
+    
+    public Point(int r, int c) {
         this.r = r;
         this.c = c;
     }
@@ -12,77 +13,84 @@ class Point {
 
 public class Main {
     static int N,L,R;
-    static int[][] maps;
+    static int[][] map;
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
-    static boolean[][] visited;
-    static boolean flag = false;
-    static int answer = 0;
-
-    public static void main(String[] args) throws Exception{
+    
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
-        maps = new int[N][N];
-
+        map = new int[N][N];
+        
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
-                maps[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
+        
+        int answer = 0;
+        
         while(true) {
-            visited = new boolean[N][N];
-            flag = false;
-            // 국경이 열리는 곳 찾기
+            boolean[][] visited = new boolean[N][N];
+            boolean stop = true;
+            
             for(int i=0; i<N; i++) {
                 for(int j=0; j<N; j++) {
-                    if(!visited[i][j])  bfs(i,j);
-                }
-            }
-
-            if(!flag) break;
-            answer++;
-
-        }
-        System.out.println(answer);
-    }
-
-    public static void bfs(int r, int c) {
-        Queue<Point> queue = new ArrayDeque<>();
-        List<Point> list = new ArrayList<>();
-        queue.offer(new Point(r,c));
-        visited[r][c] = true;
-
-        while(!queue.isEmpty()) {
-            Point p = queue.poll();
-            list.add(new Point(p.r, p.c));
-            for(int d=0; d<4; d++) {
-                int nr = p.r + dr[d];
-                int nc = p.c + dc[d];
-
-                if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc]) {
-                    if(Math.abs(maps[nr][nc] - maps[p.r][p.c]) >= L && Math.abs(maps[nr][nc] - maps[p.r][p.c]) <= R) {
-                        queue.offer(new Point(nr,nc));
-                        visited[nr][nc] = true;
+                    if(!visited[i][j]) {
+                        int count = open(i,j, visited);
+                        if(count>1) stop = false;
                     }
                 }
             }
+            
+            if(stop) break;
+            
+            answer++;
         }
-
-        if(list.size() > 1) {
-            flag = true;
-            int sum = 0;
-            for (Point p : list) {
-                sum += maps[p.r][p.c];
-            }
-            int avg = sum / list.size();
-            for (Point p : list) {
-                maps[p.r][p.c] = avg;
+        
+        
+        System.out.println(answer);
+    }
+    
+    public static int open(int r, int c, boolean[][] visited) {
+        Queue<Point> queue = new ArrayDeque<>();
+        List<Point> peoples = new ArrayList<>();
+        
+        queue.offer(new Point(r,c));
+        visited[r][c] = true;
+        
+        while(!queue.isEmpty()) {
+            Point p = queue.poll();
+            peoples.add(new Point(p.r, p.c));
+            
+            for(int d=0; d<4; d++) {
+                int nr = p.r + dr[d];
+                int nc = p.c + dc[d];
+                
+                if(nr>=0 && nr<N && nc>=0 && nc<N && !visited[nr][nc] && Math.abs(map[p.r][p.c] - map[nr][nc])>=L && Math.abs(map[p.r][p.c]- map[nr][nc])<=R) {
+                    queue.offer(new Point(nr,nc));
+                    visited[nr][nc] = true;
+                }
             }
         }
+        
+        int sum = 0;
+        
+        for(int i=0; i<peoples.size(); i++) {
+            Point p = peoples.get(i);
+            sum += map[p.r][p.c];
+        }
+        
+        int changePeoples = sum / peoples.size();
+        
+        for(Point p: peoples) {
+            map[p.r][p.c] = changePeoples;
+        }
+        
+        return peoples.size();
     }
 }
