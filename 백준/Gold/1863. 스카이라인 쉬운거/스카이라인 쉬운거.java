f93f23dffaa447f45deac3aad5cdbcb818c8eb13
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-08-07 09:32:35 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 192 ms, Memory: 20232 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -1,29 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-class Point implements Comparable<Point> {
-    int x;
-    int y;
+class Point {
+    int x,y;
     
     Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    
-    @Override
-    public int compareTo(Point p) {
-        return this.x - p.x;
-    }
 }
-
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        int n = Integer.parseInt(br.readLine());
+        
         List<Point> list = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
+        
+        int n = Integer.parseInt(br.readLine());
         int answer = 0;
         
         for(int i=0; i<n; i++) {
@@ -33,19 +28,20 @@ public class Main {
             list.add(new Point(x,y));
         }
         
-        Collections.sort(list);
-    
+        // 정렬은 되서 주어짐
+        
         for(Point p: list) {
             int x = p.x;
             int y = p.y;
+            
             while(!stack.isEmpty() && stack.peek() > y) {
                 stack.pop();
                 answer++;
             }
             
-            if ((stack.isEmpty() || stack.peek() != y) && y != 0) {
+            if((stack.isEmpty() || stack.peek() != y) && y!=0) {
                 stack.push(y);
-            }        
+            }
         }
         
         while(!stack.isEmpty()) {
