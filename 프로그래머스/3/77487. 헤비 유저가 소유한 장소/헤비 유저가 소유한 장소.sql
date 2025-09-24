632f7d49d745018365efc392faa657694f744d7d
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-09-24 16:10:30 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+--  공간을 둘 이상 등록한 사람
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(*) >= 2
+)
+ORDER BY ID
