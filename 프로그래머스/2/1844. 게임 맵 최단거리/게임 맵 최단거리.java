7f218ae74a28c70c9f60b8e76a75631f90fb836a
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-09-25 11:54:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.88 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,55 +1,55 @@
+import java.io.*;
 import java.util.*;
 
 class Point {
     int r;
     int c;
-    int count;
+    int dist;
     
-    Point(int r, int c, int count) {
+    Point(int r, int c, int dist) {
         this.r = r;
         this.c = c;
-        this.count = count;
+        this.dist = dist;
     }
 }
 
 class Solution {
-    static int n,m;
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
-    
+    static int n,m;
+        
     public int solution(int[][] maps) {
         n = maps.length;
         m = maps[0].length;
-        
-        int answer = bfs(0,0, maps);
-        return answer== -1 ? -1 : answer + 1;
+    
+        int answer = bfs(maps);
+        return answer;
     }
     
-    private static int bfs(int r, int c, int[][] maps) {
+    public static int bfs(int[][] maps) {
         Queue<Point> queue = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-        queue.offer(new Point(r,c,0));
-        visited[r][c] = true;
+        
+        queue.offer(new Point(0,0,1));
+        visited[0][0] = true;
         
         while(!queue.isEmpty()) {
             Point p = queue.poll();
-            
-            if(p.r == n-1 && p.c == m-1) {
-                return p.count;
-            }
+            if(p.r==n-1 && p.c==m-1) return p.dist;
             
             for(int d=0; d<4; d++) {
                 int nr = p.r + dr[d];
                 int nc = p.c + dc[d];
-                int nextCount = p.count + 1;
+                int dist = p.dist;
+                
                 if(nr>=0 && nr<n && nc>=0 && nc<m && !visited[nr][nc] && maps[nr][nc]==1) {
-                    queue.offer(new Point(nr,nc, nextCount));
+                    queue.offer(new Point(nr,nc, dist+1));
                     visited[nr][nc] = true;
                 }
             }
         }
-        
         return -1;
-        
     }
+    
+  
 }
