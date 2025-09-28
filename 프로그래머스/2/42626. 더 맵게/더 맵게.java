78323c918a99bc96e5cd81bc6f4ce2c19688bb37
sangzun-han <57563053+sangzun-han@users.noreply.github.com>
2025-09-28 14:28:36 +0900

    [level 2] Title: 더 맵게, Time: 1310.53 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,31 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int count = 0;
-        PriorityQueue<Long> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> a-b);
         
         for(int i=0; i<scoville.length; i++) {
-            pq.offer((long)scoville[i]);
+            pq.offer(scoville[i]);
         }
         
-        while(!pq.isEmpty() && pq.size() != 1) {
-            long food1 = pq.poll();
-            if(food1 >= K) break;
-            long food2 = pq.poll() * 2;
-            pq.offer(food1+food2);
-            count++;
+        int mixCount = 0;
+        
+        while(!pq.isEmpty() && pq.size() >=2) {
+            if(pq.peek() >= K) return mixCount;
+            int first = pq.poll();
+            int second = pq.poll() * 2;
+            int mixScoville = first + second;
+            pq.offer(mixScoville);
+            mixCount++;
         }
         
-        if(pq.size() == 1) {
-            if(pq.peek() < K) {
-                count = -1;
-            }
-        }
-        
-        if(pq.isEmpty()) {
-            count = -1;
-        }
-        int answer = count;
-        return answer;
+        if(pq.size()==1 && pq.peek() >= K) return mixCount;
+        else return -1;
     }
 }
